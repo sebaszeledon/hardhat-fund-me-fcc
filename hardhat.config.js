702560152e8d88558150128fff82f1f696aca422
sebaszeledon <sebas.zeledon@gmail.com>
2024-04-10 21:46:03 -0600

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -41,7 +41,7 @@ module.exports = {
         url: SEPOLIA_RPC_URL,
         accounts: [PRIVATE_KEY],
         chainId: 11155111,
-        blockConfirmations: 6,
+        blockConfirmations: 2,
     },
   },
   etherscan: {
